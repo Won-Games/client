1f41ad55037b93aee03621f7821c5ecb08a094cf
Willian Justen de Vasconcellos <willianjustenqui@gmail.com>
2020-08-25 19:00:25 -0300

    Configure absolute path storybook

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,6 +1,6 @@
 import { ThemeProvider } from 'styled-components'
-import GlobalStyles from '../src/styles/global'
-import theme from '../src/styles/theme'
+import GlobalStyles from 'styles/global'
+import theme from 'styles/theme'
 
 export const decorators = [
   (Story) => (
