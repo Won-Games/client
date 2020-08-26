12c5f9a35583aacdb5e3e987ad59e3205c5dd0db
Willian Justen de Vasconcellos <willianjustenqui@gmail.com>
2020-08-26 18:12:32 -0300

    Add option to hideOnMobile

--- src/components/Logo/styles.ts
+++ src/components/Logo/styles.ts
@@ -1,4 +1,5 @@
 import styled, { css } from 'styled-components'
+import media from 'styled-media-query'
 
 import { LogoProps } from '.'
 
@@ -11,13 +12,30 @@ const wrapperModifiers = {
   large: () => css`
     width: 20rem;
     height: 5.9rem;
+  `,
+
+  hideOnMobile: () => css`
+    ${media.lessThan('medium')`
+      width: 5.8rem;
+      height: 4.5rem;
+
+      svg {
+        height: 4.5rem;
+        pointer-events: none;
+      }
+
+      .text {
+        display: none;
+      }
+    `}
   `
 }
 
 export const Wrapper = styled.div<LogoProps>`
-  ${({ theme, color, size }) => css`
+  ${({ theme, color, size, hideOnMobile }) => css`
     color: ${theme.colors[color!]};
 
     ${!!size && wrapperModifiers[size]}
+    ${!!hideOnMobile && wrapperModifiers.hideOnMobile}
   `}
 `
--- src/components/Logo/test.tsx
+++ src/components/Logo/test.tsx
@@ -1,5 +1,6 @@
 import { screen } from '@testing-library/react'
 import { renderWithTheme } from 'utils/tests/helpers'
+import 'jest-styled-components'
 
 import Logo from '.'
 
@@ -30,5 +31,16 @@ describe('<Logo />', () => {
     expect(screen.getByLabelText(/Won Games/i).parentElement).toHaveStyle({
       width: '20rem'
     })
+  })
+
+  it('should render a bigger logo without text if hideOnMobile', () => {
+    renderWithTheme(<Logo hideOnMobile />)
+    expect(screen.getByLabelText(/Won Games/i).parentElement).toHaveStyleRule(
+      'width',
+      '5.8rem',
+      {
+        media: '(max-width: 768px)'
+      }
+    )
   })
 })
