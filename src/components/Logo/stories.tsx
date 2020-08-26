841e5489b0530ebe59c8a74829789be347314e26
Willian Justen de Vasconcellos <willianjustenqui@gmail.com>
2020-08-26 15:36:36 -0300

    Add args/controls to Logo stories

--- src/components/Logo/stories.tsx
+++ src/components/Logo/stories.tsx
@@ -1,9 +1,9 @@
 import { Story, Meta } from '@storybook/react/types-6-0'
-import Logo from '.'
+import Logo, { LogoProps } from '.'
 
 export default {
   title: 'Logo',
   component: Logo
 } as Meta
 
-export const Default: Story = () => <Logo />
+export const Default: Story<LogoProps> = (args) => <Logo {...args} />
