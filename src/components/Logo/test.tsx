8f4086c298c441973bc81e66d401baebe5a36aa1
Willian Justen de Vasconcellos <willianjustenqui@gmail.com>
2020-09-01 17:07:12 -0300

    Add jest-styled-components types

--- src/components/Logo/test.tsx
+++ src/components/Logo/test.tsx
@@ -1,6 +1,5 @@
 import { screen } from '@testing-library/react'
 import { renderWithTheme } from 'utils/tests/helpers'
-import 'jest-styled-components'
 
 import Logo from '.'
 
--- src/components/MediaMatch/test.tsx
+++ src/components/MediaMatch/test.tsx
@@ -1,5 +1,4 @@
 import { render, screen } from '@testing-library/react'
-import 'jest-styled-components'
 
 import MediaMatch from '.'
 
