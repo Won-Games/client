d7b005e74cff756725354b67e3c8755676f720eb
Willian Justen de Vasconcellos <willianjustenqui@gmail.com>
2020-09-09 20:23:19 -0300

    Add tests/story ribbon to banner

--- src/components/Banner/stories.tsx
+++ src/components/Banner/stories.tsx
@@ -21,3 +21,15 @@ export const Default: Story<BannerProps> = (args) => (
     <Banner {...args} />
   </div>
 )
+
+export const WithRibbon: Story<BannerProps> = (args) => (
+  <div style={{ maxWidth: '104rem' }}>
+    <Banner {...args} />
+  </div>
+)
+
+WithRibbon.args = {
+  ribbon: '20% OFF',
+  ribbonSize: 'normal',
+  ribbonColor: 'primary'
+}
--- src/components/Banner/test.tsx
+++ src/components/Banner/test.tsx
@@ -27,4 +27,24 @@ describe('<Banner />', () => {
 
     expect(container.firstChild).toMatchSnapshot()
   })
+
+  it('should render a Ribbon', () => {
+    renderWithTheme(
+      <Banner
+        {...props}
+        ribbon="My Ribbon"
+        ribbonSize="small"
+        ribbonColor="secondary"
+      />
+    )
+
+    const ribbon = screen.getByText(/My Ribbon/i)
+
+    expect(ribbon).toBeInTheDocument()
+    expect(ribbon).toHaveStyle({ backgroundColor: '#3CD3C1' })
+    expect(ribbon).toHaveStyle({
+      height: '2.6rem',
+      fontSize: '1.2rem'
+    })
+  })
 })
