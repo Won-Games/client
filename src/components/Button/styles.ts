15eb60413771189d830efa71b14b63ea9ba8d443
Willian Justen de Vasconcellos <willianjustenqui@gmail.com>
2020-09-11 11:02:23 -0300

    Fix font-family on Button

--- src/components/Button/styles.ts
+++ src/components/Button/styles.ts
@@ -41,6 +41,7 @@ export const Wrapper = styled.button<WrapperProps>`
     justify-content: center;
     background: linear-gradient(180deg, #ff5f5f 0%, #f062c0 50%);
     color: ${theme.colors.white};
+    font-family: ${theme.font.family};
     border: 0;
     cursor: pointer;
     border-radius: ${theme.border.radius};
