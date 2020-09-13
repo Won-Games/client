ad8141f64a529708cc7b9c508f9c602cd3680819
Willian Justen de Vasconcellos <willianjustenqui@gmail.com>
2020-09-13 18:30:32 -0300

    Create tests and match-media-mock to slider

--- jest.config.js
+++ jest.config.js
@@ -4,5 +4,5 @@ module.exports = {
   collectCoverage: true,
   collectCoverageFrom: ['src/**/*.ts(x)?', '!src/**/stories.tsx'],
   setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts'],
-  modulePaths: ['<rootDir>/src/']
+  modulePaths: ['<rootDir>/src/', '<rootDir>/.jest']
 }
--- src/components/Slider/test.tsx
+++ src/components/Slider/test.tsx
@@ -1,12 +1,24 @@
+import 'match-media-mock'
 import { render, screen } from '@testing-library/react'
 
 import Slider from '.'
 
 describe('<Slider />', () => {
-  it('should render the heading', () => {
-    const { container } = render(<Slider />)
+  it('should render children as slider item', () => {
+    const { container } = render(
+      <Slider settings={{ slidesToShow: 1, infinite: false }}>
+        <p>Item 1</p>
+        <p>Item 2</p>
+      </Slider>
+    )
 
-    expect(screen.getByRole('heading', { name: /Slider/i })).toBeInTheDocument()
+    expect(
+      screen.getByText(/item 1/i).parentElement?.parentElement
+    ).toHaveClass('slick-slide')
+
+    expect(
+      screen.getByText(/item 2/i).parentElement?.parentElement
+    ).toHaveClass('slick-slide')
 
     expect(container.firstChild).toMatchSnapshot()
   })
