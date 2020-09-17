6819bb5a66fab7e56f7f307dae2934f29a80b72b
Willian Justen de Vasconcellos <willianjustenqui@gmail.com>
2020-09-17 12:33:58 -0300

    Add BannerSlider css

--- src/components/BannerSlider/styles.ts
+++ src/components/BannerSlider/styles.ts
@@ -1,3 +1,58 @@
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
+import media from 'styled-media-query'
+import * as BannerStyles from 'components/Banner/styles'
 
-export const Wrapper = styled.main``
+export const Wrapper = styled.section`
+  ${({ theme }) => css`
+    .slick-dots {
+      list-style: none;
+      display: flex !important;
+      align-items: center;
+      justify-content: center;
+      margin-top: ${theme.spacings.small};
+
+      li {
+        background: ${theme.colors.white};
+        width: 1.2rem;
+        height: 1.2rem;
+        border-radius: 100%;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        margin: 0 ${theme.spacings.xxsmall};
+        cursor: pointer;
+
+        &.slick-active {
+          background: ${theme.colors.primary};
+        }
+      }
+
+      button {
+        opacity: 0;
+        width: 1.2rem;
+        height: 1.2rem;
+        cursor: pointer;
+      }
+    }
+
+    ${media.greaterThan('large')`
+      ${BannerStyles.Wrapper} {
+        max-width: 104rem;
+        margin: 0 auto;
+      }
+
+      .slick-dots {
+        position: absolute;
+        right: 0;
+        flex-direction: column;
+        height: 100%;
+        top: 0;
+        margin: 0;
+
+        li {
+          margin: ${theme.spacings.xxsmall} 0;
+        }
+      }
+    `}
+  `}
+`
--- src/components/BannerSlider/test.tsx
+++ src/components/BannerSlider/test.tsx
@@ -43,4 +43,10 @@ describe('<BannerSlider />', () => {
       screen.getByRole('heading', { name: /defy death 2/i, hidden: true })
     ).toBeInTheDocument()
   })
+
+  it('should render with the dots', () => {
+    const { container } = renderWithTheme(<BannerSlider items={items} />)
+
+    expect(container.querySelector('.slick-dots')).toBeInTheDocument()
+  })
 })
