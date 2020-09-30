74b79a0e2f388d1e6008a3fa1978305912e3a8de
Willian Justen de Vasconcellos <willianjustenqui@gmail.com>
2020-09-30 16:13:30 -0300

    Add Container component

---
+++ src/components/Container/test.tsx
@@ -0,0 +1,36 @@
+import theme from 'styles/theme'
+import { renderWithTheme } from 'utils/tests/helpers'
+import { Container } from '.'
+
+describe('<Container />', () => {
+  it('should render the heading', () => {
+    const { container } = renderWithTheme(
+      <Container>
+        <span>Won Games</span>
+      </Container>
+    )
+
+    expect(container.firstChild).toHaveStyleRule(
+      'max-width',
+      theme.grid.container
+    )
+
+    expect(container.firstChild).toMatchInlineSnapshot(`
+      .c0 {
+        max-width: 130rem;
+        margin-left: auto;
+        margin-right: auto;
+        padding-left: calc(3.2rem / 2);
+        padding-right: calc(3.2rem / 2);
+      }
+
+      <div
+        class="c0"
+      >
+        <span>
+          Won Games
+        </span>
+      </div>
+    `)
+  })
+})
