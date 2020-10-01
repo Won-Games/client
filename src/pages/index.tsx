f7522fb9d401b24dc6e15fc934ddaf3073807eca
Willian Justen de Vasconcellos <willianjustenqui@gmail.com>
2020-10-01 16:35:44 -0300

    Connect mock data to the page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,9 @@
-import Home from 'templates/Home'
+import Home, { HomeTemplateProps } from 'templates/Home'
+import bannersMock from 'components/BannerSlider/mock'
+import gamesMock from 'components/GameCardSlider/mock'
+import highlightMock from 'components/Highlight/mock'
 
-export default function Index(props: any) {
+export default function Index(props: HomeTemplateProps) {
   return <Home {...props} />
 }
 
@@ -18,7 +21,15 @@ export function getServerSideProps() {
   // retorno dos dados
   return {
     props: {
-      heading: 'Olha eu aqui!!!!! 1234 ssr'
+      banners: bannersMock,
+      newGames: gamesMock,
+      mostPopularHighlight: highlightMock,
+      mostPopularGames: gamesMock,
+      upcommingGames: gamesMock,
+      upcommingHighligth: highlightMock,
+      upcommingMoreGames: gamesMock,
+      freeGames: gamesMock,
+      freeHighligth: highlightMock
     }
   }
 }
