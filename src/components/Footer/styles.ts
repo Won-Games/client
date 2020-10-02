df94cd1023038920b09dc3ee27e99cea352738cb
Guillermo Angulo <anyermo@gmail.com>
2020-10-02 10:28:59 -0300

    Fix footer displacement

--- src/components/Footer/styles.ts
+++ src/components/Footer/styles.ts
@@ -12,7 +12,7 @@ export const Wrapper = styled.footer`
 export const Content = styled.div`
   ${({ theme }) => css`
     display: grid;
-    grid-template-columns: repeat(2, 1fr);
+    grid-template-columns: minmax(auto, 50%) 1fr;
     gap: ${theme.grid.gutter};
     margin-top: ${theme.spacings.medium};
 
@@ -31,6 +31,10 @@ export const Column = styled.div`
       text-decoration: none;
       margin-bottom: ${theme.spacings.xxsmall};
       font-size: ${theme.font.sizes.small};
+    }
+    a {
+      word-wrap: break-word;
+      overflow-wrap: break-word;
     }
     a:hover {
       text-decoration: underline;
