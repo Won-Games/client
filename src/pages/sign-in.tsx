caab231e6002eaed18a5195885ae12e6815262b6
Willian Justen de Vasconcellos <willianjustenqui@gmail.com>
2020-10-28 20:57:51 -0300

    Add Forms to SignIn/SignUp pages

--- src/pages/sign-in.tsx
+++ src/pages/sign-in.tsx
@@ -1,5 +1,10 @@
 import Auth from 'templates/Auth'
+import FormSignIn from 'components/FormSignIn'
 
 export default function SignIn() {
-  return <Auth title="Sign In">children</Auth>
+  return (
+    <Auth title="Sign In">
+      <FormSignIn />
+    </Auth>
+  )
 }
--- src/pages/sign-up.tsx
+++ src/pages/sign-up.tsx
@@ -1,5 +1,10 @@
 import Auth from 'templates/Auth'
+import FormSignUp from 'components/FormSignUp'
 
 export default function SignUp() {
-  return <Auth title="Sign Up">children</Auth>
+  return (
+    <Auth title="Sign Up">
+      <FormSignUp />
+    </Auth>
+  )
 }
