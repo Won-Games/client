89c529b90b7fd6ed9dedffcc15c459bd2720d12c
Willian Justen de Vasconcellos <willianjustenqui@gmail.com>
2020-10-28 17:41:29 -0300

    Add Auth mobile styles

--- src/templates/Auth/styles.ts
+++ src/templates/Auth/styles.ts
@@ -1,12 +1,17 @@
 import styled, { css } from 'styled-components'
+import media from 'styled-media-query'
 
 import * as HeadingStyles from 'components/Heading/styles'
 import * as LogoStyles from 'components/Logo/styles'
 
 export const Wrapper = styled.main`
   display: grid;
-  grid-template-columns: 1fr 1fr;
+  grid-template-columns: 1fr;
   height: 100vh;
+
+  ${media.greaterThan('medium')`
+    grid-template-columns: 1fr 1fr;
+  `}
 `
 
 export const BannerBlock = styled.div`
@@ -17,6 +22,10 @@ export const BannerBlock = styled.div`
     background-position: center center;
     padding: ${theme.spacings.xxlarge} ${theme.spacings.xxlarge}
       ${theme.spacings.large};
+
+    ${media.lessThan('medium')`
+      display: none;
+    `}
 
     &:after {
       content: '';
@@ -75,6 +84,10 @@ export const Content = styled.div`
 export const ContentWrapper = styled.div`
   ${({ theme }) => css`
     width: 30rem;
+
+    ${media.greaterThan('medium')`
+      width: 36rem;
+    `}
 
     ${LogoStyles.Wrapper} {
       margin: 0 auto ${theme.spacings.xxlarge};
