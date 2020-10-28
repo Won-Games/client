09520f80a790caaec31fa7faed3a6bff14d1c356
Willian Justen de Vasconcellos <willianjustenqui@gmail.com>
2020-10-28 15:46:25 -0300

    Add Auth template tests

--- src/templates/Auth/test.tsx
+++ src/templates/Auth/test.tsx
@@ -1,23 +1,39 @@
-import { render, screen } from '@testing-library/react'
+import { screen } from '@testing-library/react'
+import { renderWithTheme } from 'utils/tests/helpers'
 
 import Auth from '.'
 
 describe('<Auth />', () => {
   it('should render all components and children', () => {
-    render(
+    renderWithTheme(
       <Auth title="Auth Title">
         <input type="text" />
       </Auth>
     )
 
     // verifiquem se tem 2 logos
+    expect(screen.getAllByRole('img', { name: /won games/i })).toHaveLength(2)
 
     // verifica se tem o heading principal do banner
+    expect(
+      screen.getByRole('heading', {
+        name: /All your favorite games in one place/i
+      })
+    ).toBeInTheDocument()
 
     // verifica se tem o subtitle
+    expect(
+      screen.getByRole('heading', {
+        name: /won is the best and most complete gaming platform/i
+      })
+    ).toBeInTheDocument()
 
     // verifica se tem o title do content
+    expect(
+      screen.getByRole('heading', { name: /auth title/i })
+    ).toBeInTheDocument()
 
     // verifica se o children tá sendo renderizado
+    expect(screen.getByRole('textbox')).toBeInTheDocument()
   })
 })
