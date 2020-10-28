7c62a9c83b9706a7ad6ff9496cc9d6b917499825
Willian Justen de Vasconcellos <willianjustenqui@gmail.com>
2020-10-28 20:00:48 -0300

    Add FormSignIn styles

--- src/components/FormSignIn/styles.ts
+++ src/components/FormSignIn/styles.ts
@@ -1,7 +1,51 @@
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
+import { lighten, darken } from 'polished'
 
-export const Wrapper = styled.main``
+import * as TextFieldStyles from 'components/TextField/styles'
+import * as ButtonStyles from 'components/Button/styles'
 
-export const ForgotPassword = styled.a``
+export const Wrapper = styled.div`
+  ${({ theme }) => css`
+    ${TextFieldStyles.Wrapper} {
+      margin: ${theme.spacings.xxsmall} 0;
+    }
 
-export const FormLink = styled.div``
+    ${ButtonStyles.Wrapper} {
+      margin: ${theme.spacings.medium} auto ${theme.spacings.xsmall};
+    }
+  `}
+`
+
+export const FormLink = styled.div`
+  ${({ theme }) => css`
+    font-size: ${theme.font.sizes.small};
+    color: ${theme.colors.black};
+    text-align: center;
+
+    a {
+      color: ${theme.colors.secondary};
+      text-decoration: none;
+      border-bottom: 0.1rem solid ${theme.colors.secondary};
+      transition: color, border, ${theme.transition.fast};
+
+      &:hover {
+        color: ${darken(0.1, theme.colors.secondary)};
+        border-bottom: 0.1rem solid ${darken(0.1, theme.colors.secondary)};
+      }
+    }
+  `}
+`
+
+export const ForgotPassword = styled.a`
+  ${({ theme }) => css`
+    display: block;
+    font-size: ${theme.font.sizes.small};
+    color: ${theme.colors.black};
+    text-decoration: none;
+    text-align: right;
+
+    &:hover {
+      color: ${lighten(0.2, theme.colors.black)};
+    }
+  `}
+`
--- src/components/FormSignIn/test.tsx
+++ src/components/FormSignIn/test.tsx
@@ -5,13 +5,15 @@ import FormSignIn from '.'
 
 describe('<FormSignIn />', () => {
   it('should render the form', () => {
-    renderWithTheme(<FormSignIn />)
+    const { container } = renderWithTheme(<FormSignIn />)
 
     expect(screen.getByPlaceholderText(/email/i)).toBeInTheDocument()
     expect(screen.getByPlaceholderText(/password/i)).toBeInTheDocument()
     expect(
       screen.getByRole('button', { name: /sign in now/i })
     ).toBeInTheDocument()
+
+    expect(container.parentElement).toMatchSnapshot()
   })
 
   it('should render the forgot password link', () => {
