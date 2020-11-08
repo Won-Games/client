9af4a14801f4a8c4ac3da634e7274ab0a59d1033
Willian Justen de Vasconcellos <willianjustenqui@gmail.com>
2020-11-08 17:46:05 -0300

    Add GameDetails storybook details

--- src/components/GameDetails/stories.tsx
+++ src/components/GameDetails/stories.tsx
@@ -1,5 +1,6 @@
 import { Story, Meta } from '@storybook/react/types-6-0'
 import GameDetails, { GameDetailsProps } from '.'
+import mockGame from './mock'
 
 export default {
   title: 'Game/GameDetails',
@@ -9,14 +10,21 @@ export default {
       default: 'won-dark'
     }
   },
-  args: {
-    platforms: ['windows', 'linux', 'mac']
-  },
+  args: mockGame,
   argTypes: {
+    releaseDate: {
+      control: 'date'
+    },
     platforms: {
       control: {
         type: 'inline-check',
         options: ['windows', 'linux', 'mac']
+      }
+    },
+    genres: {
+      control: {
+        type: 'inline-check',
+        options: ['Role-playing', 'Narrative']
       }
     }
   }
