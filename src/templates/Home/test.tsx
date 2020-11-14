0fde6202808b77ee0b84d4fc3a2fafb1bcf88708
Morpa <andremorpa@gmail.com>
2020-11-14 09:47:50 +0000

    Fix upcomming to upcoming
    Fix upcomming to upcoming

--- src/templates/Home/test.tsx
+++ src/templates/Home/test.tsx
@@ -13,9 +13,9 @@ const props = {
   newGames: gamesMock,
   mostPopularHighlight: highlightMock,
   mostPopularGames: gamesMock,
-  upcommingGames: gamesMock,
-  upcommingHighlight: highlightMock,
-  upcommingMoreGames: gamesMock,
+  upcomingGames: gamesMock,
+  upcomingHighlight: highlightMock,
+  upcomingMoreGames: gamesMock,
   freeGames: gamesMock,
   freeHighlight: highlightMock
 }
