2f767d3150c856e8d6fa14dc78ae92e001d98713
Milton Carlos Katoo <mckatoo@gmail.com>
2020-11-22 13:52:09 -0300

    Ignore stories.tsx when jest is in watch mode
    In development, is it necessary to run the tests every time the stories are modified?

--- jest.config.js
+++ jest.config.js
@@ -13,5 +13,8 @@ module.exports = {
   moduleNameMapper: {
     '^styled-components':
       '<rootDir>/node_modules/styled-components/dist/styled-components.browser.cjs.js'
-  }
+  },
+  watchPathIgnorePatterns: [
+    'stories.tsx'
+  ]
 }
