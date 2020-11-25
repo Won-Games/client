ca65ceae19f4f2a553d12dd14e0810e1a5f1eef7
Willian Justen de Vasconcellos <willianjustenqui@gmail.com>
2020-11-25 16:22:16 -0300

    Add styles to GameItem

--- src/components/GameItem/styles.ts
+++ src/components/GameItem/styles.ts
@@ -1,13 +1,70 @@
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
+import media from 'styled-media-query'
 
-export const Wrapper = styled.div``
+export const Wrapper = styled.div`
+  ${({ theme }) => css`
+    padding: ${theme.spacings.small};
+    border-bottom: 0.1rem solid ${theme.colors.lightGray};
 
-export const GameContent = styled.div``
+    ${media.greaterThan('medium')`
+      display: flex;
+    `}
+  `}
+`
 
-export const ImageBox = styled.div``
+export const GameContent = styled.div`
+  display: flex;
+`
 
-export const Content = styled.div``
+export const ImageBox = styled.div`
+  ${({ theme }) => css`
+    flex-shrink: 0;
+    margin-right: 1.2rem;
+    width: 9.6rem;
+    height: 5.6rem;
 
-export const Title = styled.h3``
+    img {
+      width: 100%;
+      height: 100%;
+      object-fit: cover;
+    }
 
-export const Price = styled.p``
+    ${media.greaterThan('medium')`
+      margin-right: ${theme.spacings.xsmall};
+      width: 15rem;
+      height: 7rem;
+    `};
+  `}
+`
+
+export const Content = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+`
+
+export const Title = styled.h3`
+  ${({ theme }) => css`
+    font-size: ${theme.font.sizes.small};
+    line-height: ${theme.font.sizes.small};
+    font-weight: ${theme.font.bold};
+    color: ${theme.colors.black};
+    margin-bottom: ${theme.spacings.xxsmall};
+
+    ${media.greaterThan('medium')`
+      font-size: 2rem;
+      line-height: 2rem;
+    `};
+  `}
+`
+
+export const Price = styled.div`
+  ${({ theme }) => css`
+    color: ${theme.colors.white};
+    padding: 0.2rem ${theme.spacings.xxsmall};
+    background-color: ${theme.colors.secondary};
+    border-radius: ${theme.border.radius};
+    font-size: ${theme.font.sizes.small};
+    font-weight: ${theme.font.bold};
+  `}
+`
