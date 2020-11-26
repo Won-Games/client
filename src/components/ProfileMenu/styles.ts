0fc3ee2d96a2103b951fa099e54c45d8d31aae12
Willian Justen de Vasconcellos <willianjustenqui@gmail.com>
2020-11-26 18:04:36 -0300

    Add ProfileMenu styles and tests

--- src/components/ProfileMenu/styles.ts
+++ src/components/ProfileMenu/styles.ts
@@ -1,5 +1,48 @@
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
+import media from 'styled-media-query'
 
-export const Nav = styled.nav``
+export const Nav = styled.nav`
+  ${({ theme }) => css`
+    display: flex;
+    border-bottom: 0.1rem solid ${theme.colors.lightGray};
 
-export const Link = styled.a``
+    ${media.greaterThan('medium')`
+      flex-direction: column;
+      border: 0;
+
+      a:not(:last-child) {
+        border-bottom: 0.1rem solid ${theme.colors.lightGray};
+      }
+    `}
+  `}
+`
+
+export const Link = styled.a`
+  ${({ theme }) => css`
+    background: ${theme.colors.white};
+    color: ${theme.colors.black};
+    text-decoration: none;
+    display: flex;
+    align-items: center;
+    padding: ${theme.spacings.xsmall} ${theme.spacings.small};
+    transition: background, color, ${theme.transition.default};
+
+    &:hover {
+      background: ${theme.colors.primary};
+      color: ${theme.colors.white};
+    }
+
+    > span {
+      margin-left: ${theme.spacings.xsmall};
+    }
+
+    ${media.lessThan('medium')`
+      justify-content: center;
+      flex: 1;
+
+      > span {
+        display: none;
+      }
+    `}
+  `}
+`
--- src/components/ProfileMenu/test.tsx
+++ src/components/ProfileMenu/test.tsx
@@ -5,8 +5,16 @@ import ProfileMenu from '.'
 
 describe('<ProfileMenu />', () => {
   it('should render the menu', () => {
-    renderWithTheme(<ProfileMenu />)
+    const { container } = renderWithTheme(<ProfileMenu />)
 
-    // verificar os 4 links existentes
+    expect(
+      screen.getByRole('link', { name: /my profile/i })
+    ).toBeInTheDocument()
+
+    expect(screen.getByRole('link', { name: /my cards/i })).toBeInTheDocument()
+    expect(screen.getByRole('link', { name: /my orders/i })).toBeInTheDocument()
+    expect(screen.getByRole('link', { name: /sign out/i })).toBeInTheDocument()
+
+    expect(container.firstChild).toMatchSnapshot()
   })
 })
