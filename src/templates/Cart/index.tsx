48d1b5a3150179eeb2a58002771e9457f091a741
Jefferson <jeffersonbraster@gmail.com>
2020-12-03 19:40:52 -0300

    changed for Info from material-outlined

--- src/templates/Cart/index.tsx
+++ src/templates/Cart/index.tsx
@@ -7,7 +7,7 @@ import CartList, { CartListProps } from 'components/CartList'
 import Heading from 'components/Heading'
 import Showcase from 'components/Showcase'
 import Base from 'templates/Base'
-import { ErrorWarning } from '@styled-icons/remix-line/ErrorWarning'
+import { Info } from '@styled-icons/material-outlined/Info'
 
 import * as S from './styles'
 import Empty from 'components/Empty'
@@ -48,12 +48,12 @@ const Cart = ({
           />
         )}
         <S.Text>
-          <ErrorWarning size={18} /> Your purchase is protected by a secure
-          connection from the WON platform. By purchasing from our store you
-          agree and agree to our <a href="#">terms of use.</a> After making the
-          purchase you are entitled to a refund within a maximum of 30 days,
-          without any additional cost, as long as the download of the purchased
-          game has not occurred after your purchase.
+          <Info size={18} /> Your purchase is protected by a secure connection
+          from the WON platform. By purchasing from our store you agree and
+          agree to our <a href="#">terms of use.</a> After making the purchase
+          you are entitled to a refund within a maximum of 30 days, without any
+          additional cost, as long as the download of the purchased game has not
+          occurred after your purchase.
         </S.Text>
         <Divider />
       </Container>
