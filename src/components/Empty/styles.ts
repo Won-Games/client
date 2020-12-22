7e8245d2df90e0dbdf4cc53be4d4b66bd9957e57
diegodsgarcia <diego.ds.garcia@gmail.com>
2020-12-22 11:54:14 -0300

    Fix Empty style tag main to div

--- src/components/Empty/styles.ts
+++ src/components/Empty/styles.ts
@@ -1,6 +1,6 @@
 import styled, { css } from 'styled-components'
 
-export const Wrapper = styled.main`
+export const Wrapper = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
