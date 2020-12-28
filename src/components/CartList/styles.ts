42d93a1a5d485367a6ed2b2c8a419ace9c1a2537
diegodsgarcia <diego.ds.garcia@gmail.com>
2020-12-28 11:26:43 -0300

    Fix CartList style tag main to div

--- src/components/CartList/styles.ts
+++ src/components/CartList/styles.ts
@@ -2,7 +2,7 @@ import { tint } from 'polished'
 import styled, { css } from 'styled-components'
 import media from 'styled-media-query'
 
-export const Wrapper = styled.main`
+export const Wrapper = styled.div`
   ${({ theme }) => css`
     background: ${theme.colors.white};
     display: flex;
