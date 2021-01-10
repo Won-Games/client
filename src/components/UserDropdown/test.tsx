00872dabbd39037b0e69be94c933ab3b773ae354
Willian Justen de Vasconcellos <willianjustenqui@gmail.com>
2021-01-10 19:00:58 -0300

    Add UserDropdown tests

--- src/components/UserDropdown/test.tsx
+++ src/components/UserDropdown/test.tsx
@@ -1,9 +1,27 @@
-import { render, screen } from '@testing-library/react'
+import { screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+import { renderWithTheme } from 'utils/tests/helpers'
 
 import UserDropdown from '.'
 
 describe('<UserDropdown />', () => {
-  it('should render the username', () => {})
+  it('should render the username', () => {
+    renderWithTheme(<UserDropdown username="Willian" />)
 
-  it('should render the menu', () => {})
+    expect(screen.getByText(/willian/i)).toBeInTheDocument()
+  })
+
+  it('should render the menu', () => {
+    renderWithTheme(<UserDropdown username="Willian" />)
+
+    // open menu
+    userEvent.click(screen.getByText(/willian/i))
+
+    expect(
+      screen.getByRole('link', { name: /my profile/i })
+    ).toBeInTheDocument()
+
+    expect(screen.getByRole('link', { name: /wishlist/i })).toBeInTheDocument()
+    expect(screen.getByRole('link', { name: /sign out/i })).toBeInTheDocument()
+  })
 })
