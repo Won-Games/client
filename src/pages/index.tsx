6cf253eb2d98f6079ecabad3416f756a79d20d81
Willian Justen de Vasconcellos <willianjustenqui@gmail.com>
2021-01-21 13:38:40 -0300

    Create ApolloClient directly to home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,26 @@
+import { ApolloClient, gql, InMemoryCache } from '@apollo/client'
+
 import Home, { HomeTemplateProps } from 'templates/Home'
 import bannersMock from 'components/BannerSlider/mock'
 import gamesMock from 'components/GameCardSlider/mock'
 import highlightMock from 'components/Highlight/mock'
 
 export default function Index(props: HomeTemplateProps) {
+  const client = new ApolloClient({
+    uri: 'http://localhost:1337/graphql',
+    cache: new InMemoryCache()
+  })
+
+  client.query({
+    query: gql`
+      query getGames {
+        games {
+          name
+        }
+      }
+    `
+  })
+
   return <Home {...props} />
 }
 
