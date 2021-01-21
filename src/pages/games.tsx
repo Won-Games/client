09be670f0aa40c5ee3045fb49abe554cee8a1781
Willian Justen de Vasconcellos <willianjustenqui@gmail.com>
2021-01-21 19:59:48 -0300

    Add types to our QueryGames

--- src/pages/games.tsx
+++ src/pages/games.tsx
@@ -1,7 +1,9 @@
-import GamesTemplate, { GamesTemplateProps } from 'templates/Games'
-import filterItemsMock from 'components/ExploreSidebar/mock'
 import { initializeApollo } from 'utils/apollo'
 import { QUERY_GAMES } from 'graphql/queries/games'
+import { QueryGames, QueryGamesVariables } from 'graphql/generated/QueryGames'
+
+import GamesTemplate, { GamesTemplateProps } from 'templates/Games'
+import filterItemsMock from 'components/ExploreSidebar/mock'
 
 export default function GamesPage(props: GamesTemplateProps) {
   return <GamesTemplate {...props} />
@@ -10,7 +12,7 @@ export default function GamesPage(props: GamesTemplateProps) {
 export async function getStaticProps() {
   const apolloClient = initializeApollo()
 
-  const { data } = await apolloClient.query({
+  const { data } = await apolloClient.query<QueryGames, QueryGamesVariables>({
     query: QUERY_GAMES,
     variables: { limit: 9 }
   })
@@ -21,7 +23,7 @@ export async function getStaticProps() {
       games: data.games.map((game) => ({
         title: game.name,
         developer: game.developers[0].name,
-        img: `http://localhost:1337${game.cover.url}`,
+        img: `http://localhost:1337${game.cover!.url}`,
         price: new Intl.NumberFormat('en', {
           style: 'currency',
           currency: 'USD'
