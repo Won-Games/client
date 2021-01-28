c09f5ea97a30136123ff56e8c9d01ebdb785db79
Luiz Felicio <unifelicio@gmail.com>
2021-01-28 11:20:50 -0300

    fix: typo in apollo property name

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -8,7 +8,7 @@ import theme from 'styles/theme'
 import { useApollo } from 'utils/apollo'
 
 function App({ Component, pageProps }: AppProps) {
-  const client = useApollo(pageProps.initialApoloState)
+  const client = useApollo(pageProps.initialApolloState)
 
   return (
     <ApolloProvider client={client}>
