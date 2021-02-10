ce4dbc69583bbc28b8fa76191a08fa64875a6e49
Willian Justen de Vasconcellos <willianjustenqui@gmail.com>
2021-02-10 19:56:10 -0300

    Connect ExploreSidebar to filter logic on Games page

--- src/pages/games.tsx
+++ src/pages/games.tsx
@@ -1,27 +1,93 @@
 import { initializeApollo } from 'utils/apollo'
 import { QUERY_GAMES } from 'graphql/queries/games'
 import { QueryGames, QueryGamesVariables } from 'graphql/generated/QueryGames'
+import { parseQueryStringToWhere } from 'utils/filter'
 
 import GamesTemplate, { GamesTemplateProps } from 'templates/Games'
-import filterItemsMock from 'components/ExploreSidebar/mock'
+import { GetServerSidePropsContext } from 'next'
 
 export default function GamesPage(props: GamesTemplateProps) {
   return <GamesTemplate {...props} />
 }
 
-export async function getStaticProps() {
+export async function getServerSideProps({ query }: GetServerSidePropsContext) {
   const apolloClient = initializeApollo()
+
+  const filterPrice = {
+    title: 'Price',
+    name: 'price_lte',
+    type: 'radio',
+    fields: [
+      { label: 'Free', name: 0 },
+      { label: 'Under $50', name: 50 },
+      { label: 'Under $100', name: 100 },
+      { label: 'Under $150', name: 150 },
+      { label: 'Under $250', name: 250 },
+      { label: 'Under $500', name: 500 }
+    ]
+  }
+
+  const filterPlatforms = {
+    title: 'Platforms',
+    name: 'platforms',
+    type: 'checkbox',
+    fields: [
+      { label: 'Windows', name: 'windows' },
+      { label: 'Linux', name: 'linux' },
+      { label: 'Mac OS', name: 'mac' }
+    ]
+  }
+
+  const filterSort = {
+    title: 'Sort by price',
+    name: 'sort',
+    type: 'radio',
+    fields: [
+      { label: 'Lowest to highest', name: 'price:asc' },
+      { label: 'Highest to lowest', name: 'price:desc' }
+    ]
+  }
+
+  const filterCategories = {
+    title: 'Genres',
+    name: 'categories',
+    type: 'checkbox',
+    fields: [
+      { label: 'Action', name: 'action' },
+      { label: 'Adventure', name: 'adventure' },
+      { label: 'Sports', name: 'sports' },
+      { label: 'Puzzle', name: 'puzzle' },
+      { label: 'Horror', name: 'horror' },
+      { label: 'Platform', name: 'platform' },
+      { label: 'Fantasy', name: 'fantasy' },
+      { label: 'RPG', name: 'role-playing' },
+      { label: 'JRPG', name: 'jrpg' },
+      { label: 'Simulation', name: 'simulation' },
+      { label: 'Strategy', name: 'strategy' },
+      { label: 'Shooter', name: 'shooter' }
+    ]
+  }
+
+  const filterItems = [
+    filterSort,
+    filterPrice,
+    filterPlatforms,
+    filterCategories
+  ]
 
   await apolloClient.query<QueryGames, QueryGamesVariables>({
     query: QUERY_GAMES,
-    variables: { limit: 15 }
+    variables: {
+      limit: 15,
+      where: parseQueryStringToWhere({ queryString: query, filterItems }),
+      sort: query.sort as string | null
+    }
   })
 
   return {
     props: {
-      revalidate: 60,
       initialApolloState: apolloClient.cache.extract(),
-      filterItems: filterItemsMock
+      filterItems
     }
   }
 }
--- src/templates/Games/index.tsx
+++ src/templates/Games/index.tsx
@@ -1,25 +1,38 @@
+import { ParsedUrlQueryInput } from 'querystring'
+import { useRouter } from 'next/router'
+
 import { useQueryGames } from 'graphql/queries/games'
+import { parseQueryStringToFilter, parseQueryStringToWhere } from 'utils/filter'
 
 import Base from 'templates/Base'
 import { KeyboardArrowDown as ArrowDown } from '@styled-icons/material-outlined/KeyboardArrowDown'
 
 import ExploreSidebar, { ItemProps } from 'components/ExploreSidebar'
-import GameCard, { GameCardProps } from 'components/GameCard'
+import GameCard from 'components/GameCard'
 import { Grid } from 'components/Grid'
 
 import * as S from './styles'
 
 export type GamesTemplateProps = {
-  games?: GameCardProps[]
   filterItems: ItemProps[]
 }
 
 const GamesTemplate = ({ filterItems }: GamesTemplateProps) => {
+  const { push, query } = useRouter()
+
   const { data, loading, fetchMore } = useQueryGames({
-    variables: { limit: 15 }
+    variables: {
+      limit: 15,
+      where: parseQueryStringToWhere({ queryString: query, filterItems }),
+      sort: query.sort as string | null
+    }
   })
 
-  const handleFilter = () => {
+  const handleFilter = (items: ParsedUrlQueryInput) => {
+    push({
+      pathname: '/games',
+      query: items
+    })
     return
   }
 
@@ -30,7 +43,14 @@ const GamesTemplate = ({ filterItems }: GamesTemplateProps) => {
   return (
     <Base>
       <S.Main>
-        <ExploreSidebar items={filterItems} onFilter={handleFilter} />
+        <ExploreSidebar
+          initialValues={parseQueryStringToFilter({
+            queryString: query,
+            filterItems
+          })}
+          items={filterItems}
+          onFilter={handleFilter}
+        />
 
         {loading ? (
           <p>Loading...</p>
