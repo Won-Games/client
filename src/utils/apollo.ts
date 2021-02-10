39b01b83234769a62df8bbb6758acca9d2dc0797
Willian Justen de Vasconcellos <willianjustenqui@gmail.com>
2021-02-10 14:35:55 -0300

    Fix Apollo initialState and Cache

--- src/utils/apollo.ts
+++ src/utils/apollo.ts
@@ -2,7 +2,7 @@ import { ApolloClient, HttpLink, NormalizedCacheObject } from '@apollo/client'
 import { useMemo } from 'react'
 import apolloCache from './apolloCache'
 
-let apolloClient: ApolloClient<NormalizedCacheObject>
+let apolloClient: ApolloClient<NormalizedCacheObject | null>
 
 function createApolloClient() {
   return new ApolloClient({
@@ -12,7 +12,7 @@ function createApolloClient() {
   })
 }
 
-export function initializeApollo(initialState = {}) {
+export function initializeApollo(initialState = null) {
   // serve para verificar se já existe uma instância, para não criar outra
   const apolloClientGlobal = apolloClient ?? createApolloClient()
 
@@ -30,7 +30,7 @@ export function initializeApollo(initialState = {}) {
   return apolloClient
 }
 
-export function useApollo(initialState = {}) {
+export function useApollo(initialState = null) {
   const store = useMemo(() => initializeApollo(initialState), [initialState])
   return store
 }
--- src/utils/apolloCache.ts
+++ src/utils/apolloCache.ts
@@ -5,7 +5,7 @@ export default new InMemoryCache({
   typePolicies: {
     Query: {
       fields: {
-        games: concatPagination()
+        games: concatPagination(['where', 'sort'])
       }
     }
   }
