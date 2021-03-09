d5f41c5d4bc1ba39c1a1c9326e130a0db09e74c3
Willian Justen de Vasconcellos <willianjustenqui@gmail.com>
2021-03-09 20:19:48 -0300

    Add joi validations

---
+++ src/utils/validations/index.ts
@@ -0,0 +1,44 @@
+import { UsersPermissionsRegisterInput } from 'graphql/generated/globalTypes'
+import Joi from 'joi'
+
+const fieldsValidations = {
+  username: Joi.string().min(5).required(),
+  email: Joi.string()
+    .email({ tlds: { allow: false } })
+    .required(),
+  password: Joi.string().required(),
+  confirm_password: Joi.string()
+    .valid(Joi.ref('password'))
+    .required()
+    .messages({ 'any.only': 'confirm password does not match with password' })
+}
+
+export type FieldErrors = {
+  [key: string]: string
+}
+
+function getFieldErrors(objError: Joi.ValidationResult) {
+  const errors: FieldErrors = {}
+
+  if (objError.error) {
+    objError.error.details.forEach((err) => {
+      errors[err.path.join('.')] = err.message
+    })
+  }
+
+  return errors
+}
+
+export function signUpValidate(values: UsersPermissionsRegisterInput) {
+  const schema = Joi.object(fieldsValidations)
+
+  return getFieldErrors(schema.validate(values, { abortEarly: false }))
+}
+
+type SignInValues = Omit<UsersPermissionsRegisterInput, 'username'>
+export function signInValidate(values: SignInValues) {
+  const { email, password } = fieldsValidations
+  const schema = Joi.object({ email, password })
+
+  return getFieldErrors(schema.validate(values, { abortEarly: false }))
+}
---
+++ src/utils/validations/test.ts
@@ -0,0 +1,67 @@
+import { signInValidate, signUpValidate } from '.'
+
+describe('validations', () => {
+  describe('signInValidate()', () => {
+    it('should validate empty fields', () => {
+      const values = { email: '', password: '' }
+
+      expect(signInValidate(values)).toMatchObject({
+        email: '"email" is not allowed to be empty',
+        password: '"password" is not allowed to be empty'
+      })
+    })
+
+    it('should return invalid email error', () => {
+      const values = { email: 'invalid-email', password: '1234' }
+      expect(signInValidate(values).email).toMatchInlineSnapshot(
+        `"\\"email\\" must be a valid email"`
+      )
+    })
+  })
+
+  describe('signUpValidate()', () => {
+    it('should validate empty fields', () => {
+      const values = { username: '', email: '', password: '' }
+
+      expect(signUpValidate(values)).toMatchObject({
+        email: expect.any(String),
+        username: expect.any(String),
+        password: expect.any(String),
+        confirm_password: expect.any(String)
+      })
+    })
+
+    it('should return short username error', () => {
+      const values = { username: 'hi', email: '', password: '' }
+
+      expect(signUpValidate(values).username).toMatchInlineSnapshot(
+        `"\\"username\\" length must be at least 5 characters long"`
+      )
+    })
+
+    it('should return invalid email error', () => {
+      const values = {
+        username: 'willian',
+        email: 'invalid-email',
+        password: ''
+      }
+
+      expect(signUpValidate(values).email).toMatchInlineSnapshot(
+        `"\\"email\\" must be a valid email"`
+      )
+    })
+
+    it('should return error if password does not match with confirm password', () => {
+      const values = {
+        username: 'willian',
+        email: 'will@won.com',
+        password: '1234',
+        confirm_password: '4321'
+      }
+
+      expect(signUpValidate(values).confirm_password).toMatchInlineSnapshot(
+        `"confirm password does not match with password"`
+      )
+    })
+  })
+})
