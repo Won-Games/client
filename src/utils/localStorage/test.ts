2554f2335b2eaee63c1e9bd1a81808739793e1bd
marcoswebmasteroficial <48739175+marcoswebmasteroficial@users.noreply.github.com>
2021-03-11 20:11:44 -0300

    simplificando
    simplificando código

--- src/utils/localStorage/test.ts
+++ src/utils/localStorage/test.ts
@@ -5,11 +5,8 @@ describe('getStorageItem()', () => {
     window.localStorage.clear()
   })
   it('should return the item from localStorage', () => {
-    window.localStorage.setItem(
-      'WONGAMES_cartItems',
-      JSON.stringify(['1', '2'])
-    )
-
+   setStorageItem('cartItems', ['1', '2'])
+    
     expect(getStorageItem('cartItems')).toStrictEqual(['1', '2'])
   })
 })
@@ -20,9 +17,7 @@ describe('setStorageItem()', () => {
   })
   it('should add the item to localStorage', () => {
     setStorageItem('cartItems', ['1', '2'])
-
-    expect(window.localStorage.getItem('WONGAMES_cartItems')).toStrictEqual(
-      JSON.stringify(['1', '2'])
-    )
+    
+    expect(getStorageItem('cartItems')).toStrictEqual(['1', '2'])
   })
 })
