41d1d82a9dd128a9ecf0a663776fab7111692a78
Luiz Cláudio <luizcns18@gmail.com>
2021-03-24 10:17:59 -0300

    Remove unused icon to avoid eslint error

--- src/components/GameInfo/index.tsx
+++ src/components/GameInfo/index.tsx
@@ -1,7 +1,4 @@
-import {
-  AddShoppingCart,
-  FavoriteBorder
-} from '@styled-icons/material-outlined'
+import { FavoriteBorder } from '@styled-icons/material-outlined'
 
 import Button from 'components/Button'
 import CartButton from 'components/CartButton'
