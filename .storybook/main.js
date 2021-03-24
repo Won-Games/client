023b5cc2c156b54a0dd2e03da749a4a8a3fe5b00
Luiz Cláudio <luizcns18@gmail.com>
2021-03-24 10:58:02 -0300

    Override config to infer more props

--- .storybook/main.js
+++ .storybook/main.js
@@ -4,5 +4,15 @@ module.exports = {
   webpackFinal: (config) => {
     config.resolve.modules.push(`${process.cwd()}/src`)
     return config
+  },
+  typescript: {
+    reactDocgen: 'react-docgen-typescript',
+    reactDocgenTypescriptOptions: {
+      shouldExtractLiteralValuesFromEnum: true,
+      compilerOptions: {
+        allowSyntheticDefaultImports: false,
+        esModuleInterop: false
+      }
+    }
   }
 }
