42c5c76ee541ba107dbf5847048a9e6d55be0cce
Willian Justen de Vasconcellos <willianjustenqui@gmail.com>
2021-04-07 14:39:32 -0300

    Adjust apollo to be able to receive session via context

--- src/utils/apollo.ts
+++ src/utils/apollo.ts
@@ -11,8 +11,9 @@ function createApolloClient(session?: Session | null) {
     uri: `${process.env.NEXT_PUBLIC_API_URL}/graphql`
   })
 
-  const authLink = setContext((_, { headers }) => {
-    const authorization = session?.jwt ? `Bearer ${session?.jwt}` : ''
+  const authLink = setContext((_, { headers, session: clientSession }) => {
+    const jwt = session?.jwt || clientSession?.jwt || ''
+    const authorization = jwt ? `Bearer ${jwt}` : ''
     return { headers: { ...headers, authorization } }
   })
 
