dd69244da1059091262758e985bbbd66ea9a9b5e
Willian Justen de Vasconcellos <willianjustenqui@gmail.com>
2021-04-07 14:21:50 -0300

    Add initial structure to useWishlist

---
+++ src/hooks/use-wishlist/index.tsx
@@ -0,0 +1,48 @@
+import { GameCardProps } from 'components/GameCard'
+import { createContext, useContext } from 'react'
+
+export type WishlistContextData = {
+  items: GameCardProps[]
+  isInWishlist: (id: string) => boolean
+  addToWishlist: (id: string) => void
+  removeFromWishlist: (id: string) => void
+  loading: boolean
+}
+
+export const WishlistContextDefaultValues = {
+  items: [],
+  isInWishlist: () => false,
+  addToWishlist: () => null,
+  removeFromWishlist: () => null,
+  loading: false
+}
+
+export const WishlistContext = createContext<WishlistContextData>(
+  WishlistContextDefaultValues
+)
+
+export type WishlistProviderProps = {
+  children: React.ReactNode
+}
+
+const WishlistProvider = ({ children }: WishlistProviderProps) => {
+  const isInWishlist = (id: string) => false
+  const addToWishlist = (id: string) => {}
+  const removeFromWishlist = (id: string) => {}
+
+  return (
+    <WishlistContext.Provider
+      value={{
+        isInWishlist,
+        addToWishlist,
+        removeFromWishlist
+      }}
+    >
+      {children}
+    </WishlistContext.Provider>
+  )
+}
+
+const useWishlist = () => useContext(WishlistContext)
+
+export { WishlistProvider, useWishlist }
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -11,6 +11,7 @@ import Head from 'next/head'
 import GlobalStyles from 'styles/global'
 import theme from 'styles/theme'
 import { useApollo } from 'utils/apollo'
+import { WishlistProvider } from 'hooks/use-wishlist'
 
 function App({ Component, pageProps }: AppProps) {
   const client = useApollo(pageProps.initialApolloState)
@@ -20,24 +21,26 @@ function App({ Component, pageProps }: AppProps) {
       <ApolloProvider client={client}>
         <ThemeProvider theme={theme}>
           <CartProvider>
-            <Head>
-              <title>Won Games</title>
-              <link rel="shortcut icon" href="/img/icon-512.png" />
-              <link rel="apple-touch-icon" href="/img/icon-512.png" />
-              <link rel="manifest" href="/manifest.json" />
-              <meta
-                name="description"
-                content="The best Game Stores in the world!"
+            <WishlistProvider>
+              <Head>
+                <title>Won Games</title>
+                <link rel="shortcut icon" href="/img/icon-512.png" />
+                <link rel="apple-touch-icon" href="/img/icon-512.png" />
+                <link rel="manifest" href="/manifest.json" />
+                <meta
+                  name="description"
+                  content="The best Game Stores in the world!"
+                />
+              </Head>
+              <GlobalStyles />
+              <NextNprogress
+                color="#F231A5"
+                startPosition={0.3}
+                stopDelayMs={200}
+                height={5}
               />
-            </Head>
-            <GlobalStyles />
-            <NextNprogress
-              color="#F231A5"
-              startPosition={0.3}
-              stopDelayMs={200}
-              height={5}
-            />
-            <Component {...pageProps} />
+              <Component {...pageProps} />
+            </WishlistProvider>
           </CartProvider>
         </ThemeProvider>
       </ApolloProvider>
