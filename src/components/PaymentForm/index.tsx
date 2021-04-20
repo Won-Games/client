f1ad2ec5bc7ec4eae04e2c392bb42959f15f0e49
Willian Justen de Vasconcellos <willianjustenqui@gmail.com>
2021-04-20 13:44:34 -0300

    Add styles to CardElement

--- src/components/PaymentForm/index.tsx
+++ src/components/PaymentForm/index.tsx
@@ -23,7 +23,14 @@ const PaymentForm = () => {
         </Heading>
 
         <CardElement
-          options={{ hidePostalCode: true }}
+          options={{
+            hidePostalCode: true,
+            style: {
+              base: {
+                fontSize: '16px'
+              }
+            }
+          }}
           onChange={handleChange}
         />
 
