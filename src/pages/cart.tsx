72361b50f65f3498a5130033dd986f47ddcfbc07
Willian Justen de Vasconcellos <willianjustenqui@gmail.com>
2021-04-20 12:14:20 -0300

    Protecting /cart route

--- src/pages/cart.tsx
+++ src/pages/cart.tsx
@@ -7,13 +7,16 @@ import Cart, { CartProps } from 'templates/Cart'
 
 import itemsMock from 'components/CartList/mock'
 import cardsMock from 'components/PaymentOptions/mock'
+import protectedRoutes from 'utils/protected-routes'
+import { GetServerSidePropsContext } from 'next'
 
 export default function CartPage(props: CartProps) {
   return <Cart {...props} />
 }
 
-export async function getServerSideProps() {
-  const apolloClient = initializeApollo()
+export async function getServerSideProps(context: GetServerSidePropsContext) {
+  const session = await protectedRoutes(context)
+  const apolloClient = initializeApollo(null, session)
 
   const { data } = await apolloClient.query<QueryRecommended>({
     query: QUERY_RECOMMENDED
@@ -21,6 +24,7 @@ export async function getServerSideProps() {
 
   return {
     props: {
+      session,
       items: itemsMock,
       total: '$ 430,00',
       cards: cardsMock,
--- src/utils/protected-routes.ts
+++ src/utils/protected-routes.ts
@@ -5,10 +5,11 @@ async function protectedRoutes(context: GetServerSidePropsContext) {
   const session = await getSession(context)
 
   if (!session) {
-    context.res.writeHead(302, {
-      Location: `/sign-in?callbackUrl=${context.resolvedUrl}`
-    })
-    context.res.end()
+    context.res.setHeader(
+      'Location',
+      `/sign-in?callbackUrl=${context.resolvedUrl}`
+    )
+    context.res.statusCode = 302
   }
 
   return session
