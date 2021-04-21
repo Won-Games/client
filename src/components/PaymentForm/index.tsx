4b7ed7397a0ecdf9216d2d11e7d76416bbdfcaa5
Willian Justen de Vasconcellos <willianjustenqui@gmail.com>
2021-04-21 15:29:34 -0300

    Integrate to saveOrder in database

--- src/components/PaymentForm/index.tsx
+++ src/components/PaymentForm/index.tsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import { useRouter } from 'next/router'
 import { CardElement, useElements, useStripe } from '@stripe/react-stripe-js'
-import { StripeCardElementChangeEvent } from '@stripe/stripe-js'
+import { PaymentIntent, StripeCardElementChangeEvent } from '@stripe/stripe-js'
 import { ErrorOutline, ShoppingCart } from '@styled-icons/material-outlined'
 
 import { useCart } from 'hooks/use-cart'
@@ -9,7 +9,7 @@ import Button from 'components/Button'
 import Heading from 'components/Heading'
 
 import * as S from './styles'
-import { createPaymentIntent } from 'utils/stripe/methods'
+import { createPayment, createPaymentIntent } from 'utils/stripe/methods'
 import { Session } from 'next-auth/client'
 import { FormLoading } from 'components/Form'
 
@@ -66,6 +66,16 @@ const PaymentForm = ({ session }: PaymentFormProps) => {
     setError(event.error ? event.error.message : '')
   }
 
+  const saveOrder = async (paymentIntent?: PaymentIntent) => {
+    const data = await createPayment({
+      items,
+      paymentIntent,
+      token: session.jwt
+    })
+
+    return data
+  }
+
   const handleSubmit = async (event: React.FormEvent) => {
     event.preventDefault()
     setLoading(true)
@@ -74,6 +84,7 @@ const PaymentForm = ({ session }: PaymentFormProps) => {
     if (freeGames) {
       // salva no banco
       // bater na API /orders
+      saveOrder()
 
       // redireciona para success
       push('/success')
@@ -95,6 +106,7 @@ const PaymentForm = ({ session }: PaymentFormProps) => {
 
       // salvar a compra no banco do Strapi
       // bater na API /orders
+      saveOrder(payload.paymentIntent)
 
       // redirectionar para a página de Sucesso
       push('/success')
