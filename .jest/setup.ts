26534b4d63de669bdc57d54f1e65cec5d7d3dcbf
Willian Justen de Vasconcellos <willianjustenqui@gmail.com>
2021-04-28 18:28:10 -0300

    Add next-image mock

--- .jest/setup.ts
+++ .jest/setup.ts
@@ -1,3 +1,4 @@
+import 'next-image.mock'
 import '@testing-library/jest-dom'
 import 'jest-styled-components'
 import dotenv from 'dotenv'
--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,3 +1,4 @@
+import '../.jest/next-image.mock'
 import { addDecorator } from '@storybook/react'
 import { ThemeProvider } from 'styled-components'
 import { CartContext, CartContextDefaultValues } from 'hooks/use-cart'
