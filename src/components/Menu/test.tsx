c3d657d1343f97a483f84f2e4645bd97577faf79
Rodrigo Cândido <rodrigocan@gmail.com>
2021-05-18 13:03:10 -0300

    Fix typo in test description (#47)
    sign ir => sign in

--- src/components/Menu/test.tsx
+++ src/components/Menu/test.tsx
@@ -58,7 +58,7 @@ describe('<Menu />', () => {
     expect(screen.queryByText(/sign up/i)).not.toBeInTheDocument()
   })
 
-  it('should not show sign ir or dropdownUser if loading', () => {
+  it('should not show sign in or dropdownUser if loading', () => {
     render(<Menu username="will" loading />)
 
     expect(screen.queryByText(/my profile/i)).not.toBeInTheDocument()
