5d82a44341e1c6b10af71ecb1cd4768cbd80c8fe
Lukazovic <lucas10.vieira.lv@gmail.com>
2021-05-20 23:05:44 -0300

    Improve UX with cursor auto when dropdown is open

--- src/components/Dropdown/styles.ts
+++ src/components/Dropdown/styles.ts
@@ -14,6 +14,7 @@ export const Title = styled.div`
 
 export const Content = styled.div`
   ${({ theme }) => css`
+    cursor: auto;
     display: flex;
     flex-direction: column;
     background: ${theme.colors.white};
