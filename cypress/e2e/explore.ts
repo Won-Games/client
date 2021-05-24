8939bb44fa4aab360e68e4cdfa9561cfcc3d8dae
Willian Justen de Vasconcellos <willianjustenqui@gmail.com>
2021-05-24 15:35:15 -0300

    Add tests to sort by platform and genre and empty match

--- cypress/e2e/explore.ts
+++ cypress/e2e/explore.ts
@@ -80,4 +80,31 @@ describe('Explore Page', () => {
       cy.shouldBeLessThan(500)
     })
   });
+
+  it('should filter by platform and genre', () => {
+    cy.findByText(/windows/i).click()
+    cy.location('href').should('contain', 'platforms=windows')
+
+    cy.findByText(/linux/i).click()
+    cy.location('href').should('contain', 'platforms=linux')
+
+    cy.findByText(/mac os/i).click()
+    cy.location('href').should('contain', 'platforms=mac')
+
+    cy.findByText(/action/i).click()
+    cy.location('href').should('contain', 'categories=action')
+  });
+
+  it('should return empty when no games match', () => {
+    // clear url
+    cy.visit('/games')
+
+
+    cy.findByText(/free/i).click()
+    cy.findByText(/linux/i).click()
+    cy.findByText(/sports/i).click()
+
+    cy.getByDataCy('game-card').should('not.exist')
+    cy.findByText(/We didn't find any games with this filter/i).should('exist')
+  });
 });
