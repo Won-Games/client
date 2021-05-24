1ac6bf9ab413417ee77a723f966a6855c0590287
Willian Justen de Vasconcellos <willianjustenqui@gmail.com>
2021-05-24 13:38:31 -0300

    Start getting banner with get, findByRole and within

--- cypress/e2e/home.ts
+++ cypress/e2e/home.ts
@@ -4,5 +4,10 @@ describe('Home Page', () => {
   it('should render home sections', () => {
     // visitar a página
     cy.visit('/')
+
+    cy.get('.slick-slider').within(() => {
+      cy.findByRole('heading', { name: /cyberpunk 2077/i })
+      cy.findByRole('link', { name: /buy now/i })
+    })
   });
 });
