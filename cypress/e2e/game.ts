3ece4c37f636a9c3744322ce9b1c085621f70c9f
Willian Justen de Vasconcellos <willianjustenqui@gmail.com>
2021-05-24 19:18:43 -0300

    Fix Game page release date

--- cypress/e2e/game.ts
+++ cypress/e2e/game.ts
@@ -34,7 +34,7 @@ describe('Game Page', () => {
       cy.findByRole('heading', { name: /genres/i }).should('exist')
 
       cy.findAllByText(/cd projekt red/i).should('have.length', 2)
-      cy.findByText(/dec 8, 2020/i).should('exist')
+      cy.findByText(/dec 9, 2020/i).should('exist')
       cy.findByRole('img', { name: /windows/i }).should('exist')
       cy.findByText(/free/i).should('exist')
       cy.findByText('Role-playing / Action / Sci-fi').should('exist')
