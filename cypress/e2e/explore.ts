d045faf6d1d7bb3504506779ef328a489f3c103c
Willian Justen de Vasconcellos <willianjustenqui@gmail.com>
2021-05-24 14:37:26 -0300

    Add sort by price test

--- cypress/e2e/explore.ts
+++ cypress/e2e/explore.ts
@@ -24,4 +24,20 @@ describe('Explore Page', () => {
     cy.findByRole('button', { name: /show more/i }).click()
     cy.getByDataCy('game-card').should('have.length', 30)
   });
+
+  it('should order by price', () => {
+    cy.findByText(/lowest to highest/i).click()
+    cy.location('href').should('contain', 'sort=price%3Aasc')
+
+    cy.getByDataCy('game-card').first().within(() => {
+      cy.findByText('$0.00').should('exist')
+    })
+
+    cy.findByText(/highest to lowest/i).click()
+    cy.location('href').should('contain', 'sort=price%3Adesc')
+
+    cy.getByDataCy('game-card').first().within(() => {
+      cy.findByText('$0.00').should('not.exist')
+    })
+  });
 });
