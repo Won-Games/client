6949994d9a979c9f27fb141c9552c564e84d852e
Willian Justen de Vasconcellos <willianjustenqui@gmail.com>
2021-05-24 13:40:24 -0300

    Using click and wait to animation on banner

--- cypress/e2e/home.ts
+++ cypress/e2e/home.ts
@@ -8,6 +8,18 @@ describe('Home Page', () => {
     cy.get('.slick-slider').within(() => {
       cy.findByRole('heading', { name: /cyberpunk 2077/i })
       cy.findByRole('link', { name: /buy now/i })
+
+      cy.get('.slick-dots > :nth-child(2) > button').click()
+      cy.wait(500)
+
+      cy.findByRole('heading', { name: /horizon zero dawn/i })
+      cy.findByRole('link', { name: /buy now/i })
+
+      cy.get('.slick-dots > :nth-child(3) > button').click()
+      cy.wait(500)
+
+      cy.findByRole('heading', { name: /huge promotion!/i })
+      cy.findByRole('link', { name: /browse games/i })
     })
   });
 });
