5608d2b957573e52d96abb37c393c31c3938b54e
Guillermo Angulo <anyermo@gmail.com>
2021-05-24 13:23:03 -0300

    Add minor enhancement to Textfield on autocomplete (#56)

--- src/components/TextField/styles.ts
+++ src/components/TextField/styles.ts
@@ -37,7 +37,11 @@ export const Input = styled.input<IconPositionProps>`
     &:-webkit-autofill {
       -webkit-box-shadow: 0 0 0 ${theme.spacings.small}
         ${theme.colors.lightGray} inset;
-      filter: none
+      filter: none;
+      &::first-line {
+        font-family: ${theme.font.family};
+        font-size: ${theme.font.sizes.medium};
+      }
     }
   `}
 `
