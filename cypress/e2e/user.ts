b5577dc29ec8c759bbe926122f69f5d1e7cd0311
Willian Justen de Vasconcellos <willianjustenqui@gmail.com>
2021-05-24 15:51:18 -0300

    Add SignIn e2e test

--- cypress/e2e/user.ts
+++ cypress/e2e/user.ts
@@ -11,4 +11,18 @@ describe('User', () => {
     cy.url().should('eq', `${Cypress.config().baseUrl}/`)
     cy.findByText(user.username).should('exist')
   });
+
+  it('should sign in and sign out', () => {
+    cy.visit('/sign-in')
+
+    cy.findAllByPlaceholderText(/email/i).type('e2e@wongames.com')
+    cy.findAllByPlaceholderText(/password/i).type('123456')
+    cy.findByRole('button', { name: /sign in now/i }).click()
+
+    cy.findByText(/cypress/i).should('exist').click()
+    cy.findByText(/sign out/i).click()
+
+    cy.findByRole('link', { name: /sign in/i }).should('exist')
+    cy.findByText(/cypress/i).should('not.exist')
+  });
 });
