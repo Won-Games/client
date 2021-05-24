e8d1def04c80974589c7ef82214a229bd6181f9e
Willian Justen de Vasconcellos <willianjustenqui@gmail.com>
2021-05-24 16:37:55 -0300

    Add tests to Free Games checkout flow

---
+++ cypress/e2e/checkout.ts
@@ -0,0 +1,59 @@
+/// <reference path="../support/index.d.ts" />
+
+import { createUser, User } from "../support/generate";
+
+describe('Checkout', () => {
+  let user: User
+  before(() => {
+    user = createUser()
+  })
+
+  describe('Free Games', () => {
+    it('should buy free game', () => {
+      // visitar sign-up
+      cy.visit('/sign-up')
+
+      // preencher os dados
+      cy.signUp(user)
+
+      // redirecionar para home
+      cy.url().should('eq', `${Cypress.config().baseUrl}/`)
+
+      // abrir a página de explore
+      cy.findByRole('link', { name: /explore/i }).click()
+      cy.url().should('eq', `${Cypress.config().baseUrl}/games`)
+
+      // filtrar por jogos free
+      cy.findByText(/free/i).click()
+      cy.url().should('contain', `price_lte=0`)
+
+      // adicionar um jogo ao carrinho
+      cy.addToCartByIndex(0)
+
+      // vou para página de checkout
+      cy.findAllByLabelText(/shopping cart/i).first().click()
+      cy.getByDataCy('cart-list').within(() => {
+        cy.findByText(/buy it now/i).click()
+      })
+
+      // verificar se tá escrito que é só jogo gratuito
+      cy.findByText(/only free games, click buy and enjoy!/i).should('exist')
+
+      // buy game
+      cy.findByRole('button', { name: /buy now/i }).click()
+
+      // redirecionar para success
+      cy.url().should('eq', `${Cypress.config().baseUrl}/success`)
+
+      // buscar pelo texto de sucesso
+      cy.findByRole('heading', { name: /your purchase was successful!/i }).should('exist')
+
+      // clicar no link de orders
+      cy.findByRole('link', { name: /^orders/i }).click()
+
+      // verificar se tem um jogo comprado
+      cy.getByDataCy('game-item').should('have.length', 1)
+
+    });
+  });
+});
--- src/components/GameItem/index.tsx
+++ src/components/GameItem/index.tsx
@@ -30,7 +30,7 @@ const GameItem = ({
   const { isInCart, removeFromCart } = useCart()
 
   return (
-    <S.Wrapper>
+    <S.Wrapper data-cy="game-item">
       <S.GameContent>
         <S.ImageBox>
           <img src={img} alt={title} />
