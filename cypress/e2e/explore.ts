ce8ff4f24a73bcb7977b982ae82cd0e8da9e0cf5
Willian Justen de Vasconcellos <willianjustenqui@gmail.com>
2021-05-24 15:31:59 -0300

    Add tests to sort by price

--- cypress/e2e/explore.ts
+++ cypress/e2e/explore.ts
@@ -40,4 +40,44 @@ describe('Explore Page', () => {
       cy.shouldBeGreaterThan(0)
     })
   });
+
+  it('should filter by price', () => {
+    cy.findByText(/highest to lowest/i).click()
+
+    cy.findByText(/free/i).click()
+    cy.location('href').should('contain', 'price_lte=0')
+    cy.getByDataCy('game-card').first().within(() => {
+      cy.findByText('$0.00').should('exist')
+    })
+
+    cy.findByText('Under $50').click()
+    cy.location('href').should('contain', 'price_lte=50')
+    cy.getByDataCy('game-card').first().within(() => {
+      cy.shouldBeLessThan(50)
+    })
+
+    cy.findByText('Under $100').click()
+    cy.location('href').should('contain', 'price_lte=100')
+    cy.getByDataCy('game-card').first().within(() => {
+      cy.shouldBeLessThan(100)
+    })
+
+    cy.findByText('Under $150').click()
+    cy.location('href').should('contain', 'price_lte=150')
+    cy.getByDataCy('game-card').first().within(() => {
+      cy.shouldBeLessThan(150)
+    })
+
+    cy.findByText('Under $250').click()
+    cy.location('href').should('contain', 'price_lte=250')
+    cy.getByDataCy('game-card').first().within(() => {
+      cy.shouldBeLessThan(250)
+    })
+
+    cy.findByText('Under $500').click()
+    cy.location('href').should('contain', 'price_lte=500')
+    cy.getByDataCy('game-card').first().within(() => {
+      cy.shouldBeLessThan(500)
+    })
+  });
 });
