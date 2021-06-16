050c3071f640d7934f1b2e31f3067c0d3d957921
Willian Justen de Vasconcellos <willianjustenqui@gmail.com>
2021-06-16 19:18:06 -0300

    Use lighter react-docgen inside Storybook until they fix TypeScript compatibility
    See more: https://github.com/styleguidist/react-docgen-typescript/issues/356

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,6 +1,9 @@
 module.exports = {
   stories: ['../src/components/**/stories.tsx'],
   addons: ['@storybook/addon-essentials'],
+  typescript: {
+    reactDocgen: 'react-docgen'
+  },
   webpackFinal: (config) => {
     config.resolve.modules.push(`${process.cwd()}/src`)
     return config
