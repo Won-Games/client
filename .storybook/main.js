62a57c5ffc95ac3c6527f89a0e9b6c4d28fbce56
Willian Justen de Vasconcellos <willianjustenqui@gmail.com>
2021-06-23 16:23:46 -0300

    Update versions

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,9 +1,6 @@
 module.exports = {
   stories: ['../src/components/**/stories.tsx'],
   addons: ['@storybook/addon-essentials'],
-  typescript: {
-    reactDocgen: 'react-docgen'
-  },
   webpackFinal: (config) => {
     config.resolve.modules.push(`${process.cwd()}/src`)
     return config
