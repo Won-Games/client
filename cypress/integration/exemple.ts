f4b9e72d6359a4d566e2f79d107fbfd00a9d918b
tgsouza86 <tgsouza86@gmail.com>
2022-05-09 07:09:26 -0300

    Seven - Configuration the Eslint for the Cypress

--- cypress/integration/exemple.ts
+++ cypress/integration/exemple.ts
@@ -1,9 +1,6 @@
-/* eslint-disable @typescript-eslint/triple-slash-reference */
-/* eslint-disable prettier/prettier */
-// load type definitions from Cypress module
 ///<reference path="../support/index.d.ts"/>
 describe('Cypress TS', () => {
-    it('should go to go Google', () => {
-        cy.google
-    })
+  it('should go to go Google', () => {
+    cy.google
+  })
 })
